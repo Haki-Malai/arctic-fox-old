630384ae479d83d429c7c055a81d8b13335901a0
Haki-Malai <p17mala@ionio.gr>
2022-07-27 19:20:20 +0300

    Remove backend/static/js/main.js

--- backend/static/js/main.js
+++
@@ -1,6 +0,0 @@
-var url = new URL(window.location.href);
-var access_token = url.searchParams.get('access_token'); 
-
-function goHome() {
-    window.location = '/admin/home?access_token=' + access_token;
-}
